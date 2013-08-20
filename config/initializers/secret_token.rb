7decfd782ea41d06c75a24f1adeb58179a2cfd7f
Mark McSpadden <markmcspadden@gmail.com>
2013-08-19 20:18:44 -0500

    Rails 4 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmojiCollections::Application.config.secret_key_base = '2d7c8eae9b2e17f333d8c12d571f9a9e7a5d7a26c51b607edb03f4ca3482dcd8d0e7535df220dffc4d4e7338d147d0bf4e83deb716b320340a9fa859c6c02f33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmojiCollections::Application.config.session_store :cookie_store, key: '_emoji_collections_session'
